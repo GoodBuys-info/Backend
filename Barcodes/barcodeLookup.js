35571508bb9cc501b0e414d29169b6143b45bddd
Swathi Prabha <swathiprabhahv@gmail.com>
2021-05-18 17:21:54 -0700

    Added barcode api

--- Barcodes/barcodeLookup.js
+++ Barcodes/barcodeLookup.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.param("barcode", function (req, res, next, barcode) {
   (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto("https://barcode-database.com/UPC/" + barcode);
 
