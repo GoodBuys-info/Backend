29675be5909c0be991ea27de05fee36ed12248e5
Swathi Prabha <swathiprabhahv@gmail.com>
2021-09-14 17:37:24 -0700

    Updated application

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const express = require("express");
 //const puppeteer = require("puppeteer");
 const app = express();
 const port = 3000;
-const veganCertificateScraper = require("./vegan");
-const betterCottonScraper = require("./betterCotton");
-const birdFriendlyScraper = require("./birdfriendlycofee");
-const blueAngelScraper = require("./blueAngel");
-const cornucopiaScraper = require("./cornucopiaScore");
-const audubonScraper = require("./audubon");
 
 app.get("/", (req, res) => {
   getAllCertificateScrapers.then((certificates) => {
